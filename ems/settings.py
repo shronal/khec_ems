077e0a4aed9c9dcb48ec62f385b787526ba4ae54
shronal <shronalduwal999@gmail.com>
2025-08-01 11:08:14 +0545

    css updated

--- ems/settings.py
+++ ems/settings.py
@@ -164,7 +164,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
